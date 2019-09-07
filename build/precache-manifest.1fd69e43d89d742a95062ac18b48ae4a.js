ed1e07e7de7827e8698b854d004e5b566f893800
kor_ka <gleb.putintsev@openland.com>
2019-09-07 20:20:01 +0300

    chore(front): bump build

---
+++ build/precache-manifest.1fd69e43d89d742a95062ac18b48ae4a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3888d4e86f773bed1ad5dbf44c648c0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09400a0568c5e32e5a20",
+    "url": "/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "8ec6517fc55583a70bf5",
+    "url": "/static/js/2.1f3cab1c.chunk.js"
+  },
+  {
+    "revision": "09400a0568c5e32e5a20",
+    "url": "/static/js/main.2f332597.chunk.js"
+  },
+  {
+    "revision": "a2802ae88d98359aeafc",
+    "url": "/static/js/runtime~main.9921b5f8.js"
+  }
+]);

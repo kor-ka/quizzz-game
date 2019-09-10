142ae4ce1e3d16d73f5c89a7329f71a5cce33fb8
kor_ka <gleb.putintsev@openland.com>
2019-09-10 11:09:50 +0300

    chore(*): git clean up

--- server/dist/Game.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class GameWatcher {
-    constructor(id) {
-        this.id = id;
-    }
-}
-exports.GameWatcher = GameWatcher;
--- server/dist/utils/makeId.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.makeId = () => {
-    var text = "";
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    for (var i = 0; i < 5; i++)
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-    return text;
-};
